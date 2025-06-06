285dde57ec24b3ef61cee092b243558885659824
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-06-06 15:05:02 +0700

    install validator, add Subscription and User model

---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { isEmail } from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "User Email is required"],
+      unique: true,
+      trim: true,
+      minLength: 5,
+      maxLength: 255,
+      lowercase: true,
+      validate: {
+        validator: isEmail,
+        message: "Please fill a valid email address",
+        // isAsync: false
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "User Password is required"],
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
